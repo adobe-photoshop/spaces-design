64a33539aafc9ba0cd720997f5d1ec13a4670b13
Kaori Mikawa <kmikawa@adobe.com>
2014-09-25 11:25:10 -0700

    Added document-actions-test

--- test/specs.js
+++ test/specs.js
@@ -32,6 +32,7 @@ define(function (require, exports) {
             "test/spec/example-test",
             "test/spec/application-test",
             "test/spec/document-test",
+            "test/spec/document-actions-test",
             "jsx!test/spec/jsx/NumberInput-test"
         ],
         integration : [
