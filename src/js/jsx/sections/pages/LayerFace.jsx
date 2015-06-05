acb04a68fa75e6f9961ba12bbcc8d2719598a5c5
Eric Haseltine <ehaselti@adobe.com>
2015-06-05 11:06:39 -0400

    Fixes extra selection problem and removes extra call to Immutable.Set

--- src/js/jsx/sections/pages/LayerFace.jsx
+++ src/js/jsx/sections/pages/LayerFace.jsx
@@ -75,8 +75,7 @@ define(function (require, exports, module) {
             event.stopPropagation();
 
             // Don't select if this is the click that follows a drag operation
-            if (event.currentTarget.classList.contains("face__drag_target") &&
-                event.currentTarget.classList.contains("face__select_immediate")) {
+            if (event.currentTarget.classList.contains("face__drag_target")) {
                 return;
             }
 
