1aee18dc2ab9d6b85a1076966c521856bb1e91b4
Barkin Aygun <volfied@gmail.com>
2015-06-23 08:54:05 -0700

    Listen to move events during type tool

--- src/js/tools/type.js
+++ src/js/tools/type.js
@@ -30,6 +30,12 @@ define(function (require, exports, module) {
         Tool = require("js/models/tool");
 
     /**
+     * Layers can be moved using type tool by holding down cmd
+     * We need to reset the bounds correctly during this
+     */
+    var _moveHandler;
+
+    /**
      * @implements {Tool}
      * @constructor
      */
@@ -38,13 +44,26 @@ define(function (require, exports, module) {
             firstLaunch = true;
             
         var selectHandler = function () {
+            _moveHandler = function () {
+                var documentStore = this.flux.store("application"),
+                    currentDocument = documentStore.getCurrentDocument();
+
+                this.flux.actions.layers.resetBounds(currentDocument, currentDocument.layers.allSelected);
+            }.bind(this);
+            
+            descriptor.addListener("move", _moveHandler);
+
             if (firstLaunch) {
                 firstLaunch = false;
                 return descriptor.batchPlayObjects([resetObj]);
             }
         };
 
-        Tool.call(this, "typeCreateOrEdit", "Type", "typeCreateOrEditTool", selectHandler);
+        var deselectHandler = function () {
+            descriptor.removeListener("move", _moveHandler);
+        };
+
+        Tool.call(this, "typeCreateOrEdit", "Type", "typeCreateOrEditTool", selectHandler, deselectHandler);
 
         this.activationKey = "t";
         this.hideTransformControls = true;
