174c85448946012c011d35d5cbba9e61d2cefb26
Ian Wehrman <wehrman@adobe.com>
2015-10-18 14:23:07 -0500

    Allow global shortcuts when non-input elements are focused, but blur those elements if a shortcut is matched.

---
+++ src/js/util/dom.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2015 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+define(function (require, exports) {
+    "use strict";
+
+    /**
+     * Determines whether the given element is an HTML input element.
+     * 
+     * @param {HTMLElement} el
+     * @return {boolean}
+     */
+    var isInput = function (el) {
+        return el instanceof window.HTMLInputElement;
+    };
+
+    /**
+     * Determines whether the given input element is a textual element.
+     * 
+     * @param {HTMLInputElement} el
+     * @return {boolean}
+     */
+    var isTextInput = function (el) {
+        switch (el.type) {
+            case "text":
+            case "search":
+            case "url":
+            case "tel":
+            case "password":
+                return true;
+            default:
+                return false;
+        }
+    };
+
+    exports.isInput = isInput;
+    exports.isTextInput = isTextInput;
+});
