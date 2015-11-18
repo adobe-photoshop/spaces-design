de8aead082439d2e9f0f7783e0b36d2970cb8b2e
Barkin Aygun <volfied@gmail.com>
2015-11-18 08:42:37 -0800

    JSDoc fixes

--- src/js/jsx/shared/TextInput.jsx
+++ src/js/jsx/shared/TextInput.jsx
@@ -50,7 +50,8 @@ define(function (require, exports, module) {
         mixins: [Focusable, FluxMixin],
 
         /**
-         * Once after focus, whether to suppress mouseup to maintain the initial selection.
+         * Once after focus, whether to suppress mouseup
+         * to maintain the initial selection.
          *
          * @private
          * @type {boolean}
