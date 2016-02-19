8b3d852e6c9e7ecdd564d700668e8ab71ca63242
Shaoshing <shaoshing@me.com>
2016-02-19 11:37:05 -0800

    Use internal descriptor ID in the console log.

--- src/js/actions/debug.js
+++ src/js/actions/debug.js
@@ -31,6 +31,14 @@ define(function (require, exports) {
         locks = require("js/locks");
 
     /**
+     * Highlight descritor in the console that take longer than the specific time (ms) to complete.
+     * 
+     * @const
+     * @type {number}
+     */
+    var SLOW_DESCRIPTOR = 100;
+
+    /**
      * Temporary helper function to easily open the testrunner. This should
      * eventually replaced with a action that opens the testrunner in a new
      * window.
@@ -393,8 +401,8 @@ define(function (require, exports) {
 
         var blockStyle = "color:gray;",
             groupStyle = "color:blue;font-weight:normal",
-            // Each descriptor log has a unique ID to help mapping between console message and timeline stamp.
-            descritorID = 500;
+            redText = "color:red;font-weight:normal",
+            blueText = "color:blue;font-weight:normal";
 
         descriptor.__getAsync = descriptor.__getAsync || descriptor._getAsync;
         descriptor.__batchPlayAsync = descriptor.__batchPlayAsync || descriptor._batchPlayAsync;
@@ -404,54 +412,114 @@ define(function (require, exports) {
         };
 
         if (enabled) {
+            // Customized version of the `descriptor._getAsync` to capture the descriptor ID.
+            var getAsync = Promise.promisify(function (reference, options, idCallback, callback) {
+                var id = this.get(reference, options, callback);
+                idCallback(id);
+                return id;
+            }, {
+                context: window._spaces.ps.descriptor
+            });
+            
             descriptor._getAsync = function (reference, options) {
-                var id = descritorID++,
-                    start = new Date();
+                var start = new Date(),
+                    descriptorID;
 
-                log.groupCollapsed("%c[Descriptor] Executing get - %d", groupStyle, id);
-                log.trace("Trace");
-                log.debug("Params:\n%c%s", blockStyle, JSON.stringify(reference, null, " "));
-                log.groupEnd();
-                log.timeStamp("[Descriptor] Executing get - " + id);
+                return getAsync(reference, options, function (id) {
+                    descriptorID = id;
 
-                return descriptor.__getAsync(reference, options).then(function (result) {
-                    var end = new Date();
-                    
-                    log.groupCollapsed("%c[Descriptor] Finished get in %dms - %d", groupStyle, end - start, id);
+                    log.groupCollapsed("%c[Descriptor] Executing get - %d", groupStyle, descriptorID);
                     log.trace("Trace");
+                    log.debug("Params:\n%c%s", blockStyle, JSON.stringify(reference, null, " "));
+                    log.groupEnd();
+                    log.timeStamp("[Descriptor] Executing get - " + descriptorID);
+                }).then(function (result) {
+                    var end = new Date();
+
+                    log.groupCollapsed("%c[Descriptor] Finished get in %c%dms%c - %d", groupStyle,
+                        end - start > SLOW_DESCRIPTOR ? redText : blueText,
+                        end - start,
+                        blueText,
+                        descriptorID);
                     log.debug("Result:\n%c%s", blockStyle, JSON.stringify(result, null, " "));
                     log.groupEnd();
-                    log.timeStamp("[Descriptor] Finished get - " + id);
+                    log.timeStamp("[Descriptor] Finished get - " + descriptorID);
                     return result;
                 });
             };
 
-            descriptor._batchPlayAsync = function (commands, options) {
-                var commandNames = commands.map(function (c) { return c.name; }).join(", "),
-                    isHitTest = commandNames === "hitTest",
-                    id = descritorID++,
-                    start = new Date();
+            // Customized version of the `descriptor._batchPlay` to capture the descriptor ID.
+            var batchPlayAsync = Promise.promisify(function (commands, options, idCallback, callback) {
+                var id = this.batchPlay(commands, options, callback);
+                idCallback(id);
+                return id;
+            }, {
+                context: window._spaces.ps.descriptor,
+                multiArgs: true
+            });
 
-                if (!isHitTest) {
-                    var str = "(" + JSON.stringify(commands, null, " ") + ", " +
-                        JSON.stringify(options, null, " ") + ");";
-
-                    log.groupCollapsed("%c[Descriptor] Executing batchPlay: %s - %d", groupStyle, commandNames, id);
-                    log.trace("Trace");
-                    log.debug("Params:\n%c%s", blockStyle, str);
-                    log.groupEnd();
-                    log.timeStamp("[Descriptor] Executing batchPlay - " + id);
+            var summarizeCommands = function (commands) {
+                if (commands.length === 0) {
+                    return "EMPTY";
                 }
-                return descriptor.__batchPlayAsync(commands, options).tap(function (result) {
+
+                commands = commands.map(function (c) { return c.name; });
+
+                var lastCommand = commands[0],
+                    grouppedCommands = [],
+                    results = [];
+
+                commands.forEach(function (command) {
+                    if (lastCommand !== command) {
+                        results.push(grouppedCommands);
+                        lastCommand = command;
+                        grouppedCommands = [];
+                    }
+
+                    grouppedCommands.push(command);
+                });
+
+                results.push(grouppedCommands);
+
+                return results.map(function (grouppedCommands) {
+                    return grouppedCommands.length === 1 ? grouppedCommands[0] :
+                        grouppedCommands[0] + " x " + grouppedCommands.length;
+                }).join(", ");
+            };
+
+            descriptor._batchPlayAsync = function (commands, options) {
+                var commandNames = summarizeCommands(commands),
+                    isHitTest = commandNames === "[hitTest]",
+                    start = new Date(),
+                    descriptorID;
+
+                return batchPlayAsync(commands, options, function (id) {
+                    descriptorID = id;
+
+                    if (!isHitTest) {
+                        var str = "(" + JSON.stringify(commands, null, " ") + ", " +
+                            JSON.stringify(options, null, " ") + ");";
+
+                        log.groupCollapsed("%c[Descriptor] Executing batchPlay: [%s] - %d", groupStyle,
+                            commandNames, descriptorID);
+                        log.trace("Trace");
+                        log.debug("Params:\n%c%s", blockStyle, str);
+                        log.groupEnd();
+                        log.timeStamp("[Descriptor] Executing batchPlay - " + descriptorID);
+                    }
+                }).tap(function (result) {
                     if (!isHitTest) {
                         var end = new Date();
 
-                        log.groupCollapsed("%c[Descriptor] Finished batchPlay: %s in %dms - %d", groupStyle,
-                            commandNames, end - start, id);
-                        log.trace("Trace");
+                        log.groupCollapsed("%c[Descriptor] Finished batchPlay: [%s] in %c%dms%c - %d", groupStyle,
+                            commandNames,
+                            end - start > SLOW_DESCRIPTOR ? redText : blueText,
+                            end - start,
+                            blueText,
+                            descriptorID);
                         log.debug("Result:\n%c%s", blockStyle, JSON.stringify(result, null, " "));
                         log.groupEnd();
-                        log.timeStamp("[Descriptor] Finished batchPlay - " + id);
+                        log.timeStamp("[Descriptor] Finished batchPlay - " + descriptorID);
                     }
                 });
             };
